e9e7f97ff0a8619b4b6c2fc58bbc6c25f287d2e5
Trixi <torok.beatrix@gb-art.hu>
2020-09-15 10:43:25 +0200

    footer

--- application/themes/base/elements/footer/types/default.php
+++ application/themes/base/elements/footer/types/default.php
@@ -1,7 +1,29 @@
 <footer class="footer">
     <div class="footer-top">
-        <div class="container justify-content-center">
-            <?php $area = new GlobalArea('Footer Content'); ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center col-md-3 text-md-left">
+                    <?php $area = new GlobalArea('Footer Column 1'); ?>
+                    <?php $area->display(); ?>
+                </div>
+                <div class="col-4 col-md-3">
+                    <?php $area = new GlobalArea('Footer Column 2'); ?>
+                    <?php $area->display(); ?>
+                </div>
+                <div class="col-4 col-md-3">
+                    <?php $area = new GlobalArea('Footer Column 3'); ?>
+                    <?php $area->display(); ?>
+                </div>
+                <div class="col-4 col-md-3">
+                    <?php $area = new GlobalArea('Footer Column 4'); ?>
+                    <?php $area->display(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <div class="container d-block d-md-flex align-items-center justify-content-between">
+            <?php $area = new GlobalArea('Footer Bottom'); ?>
             <?php $area->display(); ?>
         </div>
     </div>
