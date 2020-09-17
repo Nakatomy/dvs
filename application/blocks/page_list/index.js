636ed4bd3101cf5a5df169135e0c74c8b0bb5bfb
Trixi <torok.beatrix@gb-art.hu>
2020-09-17 14:02:42 +0200

    home media list

--- application/blocks/page_list/index.js
+++ application/blocks/page_list/index.js
@@ -1,8 +1,30 @@
 import './index.scss'
 
 import './templates/card/template.js'
+import './templates/home_media/template.js'
 
 // --- Ready
 $(function() {
+
+  $('.filter-menu p').each(function() {
+
+    $('.filter-menu p:first-child').addClass('active');
+
+    if($(this).hasClass('active')){
+      var filtertag = $(this).attr('id');
+      $('.home-media-list-item').show();
+      $('.home-media-list-item:not(.' + filtertag + ')').hide();
+    }
+
+
+    $(this).click(function(){
+      $('.filter-menu p').removeClass('active');
+      $(this).addClass('active');
+      var filtertag = $(this).attr('id');
+      $('.home-media-list-item').show();
+      $('.home-media-list-item:not(.' + filtertag + ')').hide();
+    });
+  })
+
 
 });
