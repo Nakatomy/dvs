3a59817cbfb6e18e620302ec871570e55de1632c
Trixi <torok.beatrix@gb-art.hu>
2020-09-17 14:45:18 +0200

    media list in progress

--- application/blocks/page_list/index.js
+++ application/blocks/page_list/index.js
@@ -2,31 +2,11 @@ import './index.scss'
 
 import './templates/card/template.js'
 import './templates/home_media/template.js'
+import './templates/media_list/template.js'
 import './templates/case_studies/template.js'
-import './templates/case_studies_simple/template.js'
+//import './templates/case_studies_extra/template.js'
 
 // --- Ready
 $(function() {
-
-  $('.filter-menu p').each(function() {
-
-    $('.filter-menu p:first-child').addClass('active');
-
-    if($(this).hasClass('active')){
-      var filtertag = $(this).attr('id');
-      $('.home-media-list-item').show();
-      $('.home-media-list-item:not(.' + filtertag + ')').hide();
-    }
-
-
-    $(this).click(function(){
-      $('.filter-menu p').removeClass('active');
-      $(this).addClass('active');
-      var filtertag = $(this).attr('id');
-      $('.home-media-list-item').show();
-      $('.home-media-list-item:not(.' + filtertag + ')').hide();
-    });
-  })
-
 
 });
--- application/blocks/page_list/templates/home_media/template.js
+++ application/blocks/page_list/templates/home_media/template.js
@@ -3,4 +3,24 @@ import './template.scss'
 // --- Ready
 $(function() {
 
+  $('.filter-menu p').each(function() {
+
+    $('.filter-menu p:first-child').addClass('active');
+
+    if($(this).hasClass('active')){
+      var filtertag = $(this).attr('id');
+      $('.home-media-list-item').show();
+      $('.home-media-list-item:not(.' + filtertag + ')').hide();
+    }
+
+
+    $(this).click(function(){
+      $('.filter-menu p').removeClass('active');
+      $(this).addClass('active');
+      var filtertag = $(this).attr('id');
+      $('.home-media-list-item').show();
+      $('.home-media-list-item:not(.' + filtertag + ')').hide();
+    });
+  })
+
 });
