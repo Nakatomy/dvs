1f32eafeb961e50215646dcbb425fd5bb337395d
bbalint <baar.balint@gb-art.hu>
2020-09-18 10:34:56 +0200

    add 404

--- application/themes/base/page_not_found.php
+++ application/themes/base/page_not_found.php
@@ -1,10 +1,18 @@
-<?php $this->inc('elements/header/view.php'); ?>
+<?php $this->inc('elements/header/view.php', [
+    'hide_section' => true
+]); ?>
 
-<div class="contents">
+<div class="contents page-not-found relative d-flex justify-content-center align-items-center">
+    <div class="logo">
+        <a href="./"><img src="<?php echo $this->getThemePath(); ?>/assets/img/dvs-logo.svg" alt=""></a>
+    </div>
     <div class="texts text-center">
-        <h1><?php echo t("Error - 404"); ?></h1>
-        <h3><?php echo t("Page not found"); ?></h3>
+        <img class="four-oh-four" src="<?php echo $this->getThemePath(); ?>/assets/img/404.svg" alt="404">
+        <h6><?php echo t("Sorry, we couldn’t find this page."); ?></h6>
+        <a href="./" class="btn" title="<?php echo t('Go to the Home page'); ?>"><?php echo t('Go to the Home page'); ?></a>
     </div>
 </div>
 
-<?php $this->inc('elements/footer/view.php'); ?>
+<?php $this->inc('elements/footer/view.php', [
+    'hide_section' => true
+]); ?>
