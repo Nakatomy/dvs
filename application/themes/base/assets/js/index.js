5c6a102ae7fa28925914c913da57735c070a36a7
Trixi <torok.beatrix@gb-art.hu>
2020-09-22 16:05:14 +0200

    infograph animation mod

--- application/themes/base/assets/js/index.js
+++ application/themes/base/assets/js/index.js
@@ -407,7 +407,7 @@ function infoGraph(){
     speed: 500,
     fade: true,
     cssEase: 'linear',
-    autoplaySpeed: 6000,
+    autoplaySpeed: 4000,
     arrows: false,
     dots: false,
     pauseOnHover: false,
