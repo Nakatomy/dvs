91c323c7bffbc0c0c0ec00e89555a54085040ea9
Trixi <torok.beatrix@gb-art.hu>
2020-09-24 14:06:11 +0200

    hero animation

--- application/blocks/hero/templates/home_hero/template.js
+++ application/blocks/hero/templates/home_hero/template.js
@@ -1,1 +1,11 @@
 import './template.scss';
+
+$(window).on('load', function(){
+
+  $('.home-hero-content .texts p:last-child').each(function(){
+    $(this).html(
+      $(this).text().replace(/(^|<\/?[^>]+>|\s+)([^\s<]+)/g, '$1<span>$2</span>')
+    );
+  });
+});
+
