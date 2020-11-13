b152f1413cda3e84df1b0d6f576123ec91c7ed47
Trixi <torok.beatrix@gb-art.hu>
2020-11-13 15:25:24 +0100

    #64974 - Ki-bekapcsolható cookie bar a C5 projektekbe

--- application/themes/base/elements/header/view.php
+++ application/themes/base/elements/header/view.php
@@ -18,6 +18,50 @@
             var $htmlClasses = document.getElementById('html');
             $htmlClasses.className = $htmlClasses.className.replace(/(\s|^)no-js(\s|$)/,' ').replace(/(\s$)(^\s)/,'');
         </script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tarteaucitronjs/1.8.3/css/tarteaucitron.min.css" integrity="sha512-FFOblsE1xWaJLWIWiI/AkEffGqv2OEQIGR1ghhEOnLKGPTed8Mhd0F/bsF+wcPduHyvp+WLCApQ6PLJ9WbALDQ==" crossorigin="anonymous" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tarteaucitronjs/1.8.3/tarteaucitron.js"></script>
+
+        <script type="text/javascript">
+            tarteaucitron.init({
+            "privacyUrl": "", /* Privacy policy url */
+
+            "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+            "cookieName": "tarteaucitron", /* Cookie name */
+
+            "orientation": "middle", /* Banner position (top - bottom) */
+
+            "showAlertSmall": false, /* Show the small banner on bottom right */
+            "cookieslist": false, /* Show the cookie list */
+
+            "showIcon": true, /* Show cookie icon to manage cookies */
+            "iconPosition": "BottomLeft", /* BottomRight, BottomLeft, TopRight and TopLeft */
+
+            "adblocker": false, /* Show a Warning if an adblocker is detected */
+
+            "DenyAllCta" : true, /* Show the deny all button */
+            "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+            "highPrivacy": true, /* HIGHLY RECOMMANDED Disable auto consent */
+
+            "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+
+            "removeCredit": false, /* Remove credit link */
+            "moreInfoLink": true, /* Show more info link */
+
+            "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+            "useExternalJs": false, /* If false, the tarteaucitron.js file will be loaded */
+
+            //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
+
+            "readmoreLink": "", /* Change the default readmore link */
+
+            "mandatory": true, /* Show a message about mandatory cookies */
+            });
+        </script>
+        <script type="text/javascript">
+            tarteaucitron.user.gajsUa = 'UA-XXXXXXXX-X';
+            tarteaucitron.user.gajsMore = function () { /* add here your optionnal _ga.push() */ };
+            (tarteaucitron.job = tarteaucitron.job || []).push('gajs');
+        </script>
     </head>
     <body class="<?php echo Package::getByHandle('base')->getDatabaseConfig()->get('layout.header_position'); ?>">
         <?php View::element('seo/after_body_open', [], $theme->getPackageHandle()); ?>
