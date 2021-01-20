c713be5026373f9b6b7c1a637dd53c0100340ddc
Alexei <alexei.sliusarev@gmail.com>
2021-01-20 17:49:56 +0200

    new navigation config structure

---
+++ configs/navigation.js
@@ -0,0 +1,63 @@
+export default [{
+    'productOverview': {
+        'title': 'Product Overview',
+        'links': [{
+                'title': 'Solution',
+                'href': '/solution'
+            },
+            {
+                'title': 'Services',
+                'href': '/services'
+            },
+            {
+                'title': 'REST API',
+                'href': '/api'
+            }
+        ]
+    },
+    'legalCorner': {
+        'title': 'Legal Corner',
+        'links': [{
+            'title': 'Legal',
+            'href': '/legal'
+        }]
+    },
+    'aboutUs': {
+        'title': 'About Us',
+        'links': [{
+                'title': 'Partners',
+                'href': '/partners'
+            },
+            {
+                'title': 'Advisory Board',
+                'href': '/advisory-board'
+            },
+            {
+                'title': 'Team',
+                'href': '/our-team'
+            },
+            {
+                'title': 'News',
+                'href': '/news'
+            },
+            {
+                'title': 'Contacts',
+                'href': '/contacts'
+            }
+        ]
+    },
+
+    'help': {
+        'title': 'Help',
+        'links': [{
+                'title': 'Privacy Policy',
+                'href': '/privacy-policy'
+            },
+            {
+                'title': 'Imprint',
+                'href': '/imprint'
+            }
+        ]
+    }
+
+}]
