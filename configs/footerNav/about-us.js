7991296d29f02d9b73cc806f03d57f6d230a280d
Alexei <alexei.sliusarev@gmail.com>
2021-01-20 17:16:04 +0200

    navigation.js started

--- configs/footerNav/about-us.js
+++ configs/footerNav/about-us.js
@@ -19,4 +19,67 @@ export default [
         title: 'Contacts',
         href: '/contacts'
     },
-    ]
+]
+    
+
+aboutUs = {
+    'title': 'About Us',
+    'links': {
+        
+        'partners': {
+            title: 'Partners',
+            href: '/partners' 
+        },
+
+        'advisoryBoard' :   {
+            title: 'Advisory Board',
+            href: '/advisory-board'
+        },
+        'team':
+           {
+        title: 'Team',
+        href: '/our-team'
+    },
+
+        'News': {
+            title: 'News',
+            href: '/news' 
+        },
+
+        'Contacts': {
+            title: 'Contacts',
+            href: '/contacts' 
+            }
+     }
+}
+    
+
+productOverview = {
+    'partners': {
+        title: 'Partners',
+        href: '/partners' 
+    },
+    'News': {
+        title: 'News',
+        href: '/news' 
+    },
+     'Contacts': {
+        title: 'Contacts',
+        href: '/contacts' 
+    }
+    }
+
+
+ 'product-overview': {
+        title: 'Partners',
+        href: '/partners' 
+    }
+
+    myObj = {
+  "name":"John",
+  "age":30,
+  "cars": {
+    "car1":"Ford",
+    "car2":"BMW",
+    "car3":"Fiat"
+  }
---
+++ configs/footerNav/navigation.js
@@ -0,0 +1,45 @@
+export default [
+    aboutUs = {
+        'title': 'About Us',
+        'links': {
+            'partners': {
+                title: 'Partners',
+                href: '/partners' 
+            },
+            'advisoryBoard': {
+                title: 'Advisory Board',
+                href: '/advisory-board'
+            },
+            'team': {
+                title: 'Team',
+                href: '/our-team'
+            },
+            'news': {
+                title: 'News',
+                href: '/news' 
+            },
+            'contacts': {
+                title: 'Contacts',
+                href: '/contacts' 
+                }
+        }
+    }, 
+
+    productOverview = {
+        'title': 'Product Overview',
+        'links': {
+            'solution': {
+                title: 'Solution',
+                href: '/solution'
+            },
+            'services': {
+                title: 'Services',
+                href: '/services'
+            },
+            'restApi': {
+                title: 'REST API',
+                href: '/api'
+            }
+        }
+    }
+]
