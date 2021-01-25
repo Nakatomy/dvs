6f37024b40c26989ae43b58061c3ab4bd7915f37
Lia <liacicati@gmail.com>
2021-01-25 18:17:42 +0100

    added Container in footer component

--- components/footer/index.js
+++ components/footer/index.js
@@ -7,117 +7,120 @@ import navProduct from "configs/navigation/productOverview";
 import navAbout from "configs/navigation/aboutUs";
 import navHelp from "configs/navigation/help";
 import navLegalCorner from "configs/navigation/legalCorner";
-
-
+import { Container } from "components";
 
 const Footer = () => {
+  const br = `\n`;
   return (
     <footer className={styles.footer}>
-      <nav className={styles.footer__grid}>
-        <div className={cn(styles.footer__item, styles.footer__line)}>
-          <div className={styles.footer__item}>
+      <Container modifier="full-width">
+        <nav className={styles.footer__grid}>
+          <div className={cn(styles.footer__item, styles.footer__line)}>
+            <div className={styles.footer__item}>
+              <div className={styles.footer__contact}>
+                <LinkComponent href="/">
+                  <Logo className={styles.footer__logo}></Logo>
+                </LinkComponent>
+              </div>
+            </div>
             <div className={styles.footer__contact}>
-              <LinkComponent href="/">
-                <Logo className={styles.footer__logo}></Logo>
-              </LinkComponent>
+              <p className={styles.footer__heading}>
+                Digital Vault Services GmbH <br />
+                Handelsregister- HRB 249553
+              </p>
+              <LinkComponent
+                title="+49 175 8868021"
+                href="tel:+491758868021"
+                className={styles.footer__link}
+              ></LinkComponent>
+            </div>
+
+            <div className={styles.footer__contact}>
+              <LinkComponent
+                title="contact@digitalvaultservices.com"
+                href="mailto:contact@digitalvaultservices.com"
+                className={styles.footer__link}
+              ></LinkComponent>
+            </div>
+            <div className={styles.footer__contact}>
+              <LinkComponent
+                title=" Stahlgruberring 43, 81829 Munich"
+                href="https://goo.gl/maps/7shPSvq8q65kGU5W9"
+                className={styles.footer__link}
+              ></LinkComponent>
             </div>
           </div>
-          <div className={styles.footer__contact}>
-            <p className={styles.footer__heading}>
-              Digital Vault Services GmbH Handelsregister - HRB 249553
-            </p>
-            <LinkComponent
-              title="+49 175 8868021"
-              href="tel:+491758868021"
-              className={styles.footer__link}
-            ></LinkComponent>
-          </div>
+          <div className={styles.footer__container}>
+            <div className={styles.footer__item}>
+              <p className={styles.footer__heading}>Product Overview</p>
+              <ul className={styles.footer__ul}>
+                {navProduct.map((item) => {
+                  return (
+                    <li key={item.id} className={styles["footer__list-item"]}>
+                      <LinkComponent
+                        title={item.title}
+                        href={item.href}
+                        className={styles.footer__link}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
 
-          <div className={styles.footer__contact}>
-            <LinkComponent
-              title="contact@digitalvaultservices.com"
-              href="mailto:contact@digitalvaultservices.com"
-              className={styles.footer__link}
-            ></LinkComponent>
-          </div>
-          <div className={styles.footer__contact}>
-            <LinkComponent
-              title=" Stahlgruberring 43, 81829 Munich"
-              href="https://goo.gl/maps/7shPSvq8q65kGU5W9"
-              className={styles.footer__link}
-            ></LinkComponent>
-          </div>
-        </div>
-        <div className={styles.footer__container}>
-          <div className={styles.footer__item}>
-            <p className={styles.footer__heading}>Product Overview</p>
-            <ul className={styles.footer__ul}>
-              {navProduct.map((item) => {
-                return (
-                  <li key={item.id} className={styles["footer__list-item"]}>
-                    <LinkComponent
-                      title={item.title}
-                      href={item.href}
-                      className={styles.footer__link}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+            <div className={styles.footer__item}>
+              <p className={styles.footer__heading}>Legal Corner</p>
+              <ul className={styles.footer__ul}>
+                {navLegalCorner.map((item) => {
+                  return (
+                    <li key={item.id} className={styles["footer__list-item"]}>
+                      <LinkComponent
+                        title={item.title}
+                        href={item.href}
+                        className={styles.footer__link}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
 
-          <div className={styles.footer__item}>
-            <p className={styles.footer__heading}>Legal Corner</p>
-            <ul className={styles.footer__ul}>
-              {navLegalCorner.map((item) => {
-                return (
-                  <li key={item.id} className={styles["footer__list-item"]}>
-                    <LinkComponent
-                      title={item.title}
-                      href={item.href}
-                      className={styles.footer__link}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+            <div className={styles.footer__item}>
+              <p className={styles.footer__heading}>About Us</p>
+              <ul className={styles.footer__ul}>
+                {navAbout.map((item) => {
+                  return (
+                    <li key={item.id} className={styles["footer__list-item"]}>
+                      <LinkComponent
+                        title={item.title}
+                        href={item.href}
+                        className={styles.footer__link}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
 
-          <div className={styles.footer__item}>
-            <p className={styles.footer__heading}>Product Overview</p>
-            <ul className={styles.footer__ul}>
-              {navAbout.map((item) => {
-                return (
-                  <li key={item.id} className={styles["footer__list-item"]}>
-                    <LinkComponent
-                      title={item.title}
-                      href={item.href}
-                      className={styles.footer__link}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
+            <div className={styles.footer__item}>
+              <p className={styles.footer__heading}>Help</p>
+              <ul className={styles.footer__ul}>
+                {navHelp.map((item) => {
+                  return (
+                    <li key={item.id} className={styles["footer__list-item"]}>
+                      <LinkComponent
+                        title={item.title}
+                        href={item.href}
+                        className={styles.footer__link}
+                      />
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
-
-          <div className={styles.footer__item}>
-            <p className={styles.footer__heading}>Product Overview</p>
-            <ul className={styles.footer__ul}>
-              {navHelp.map((item) => {
-                return (
-                  <li key={item.id} className={styles["footer__list-item"]}>
-                    <LinkComponent
-                      title={item.title}
-                      href={item.href}
-                      className={styles.footer__link}
-                    />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        </div>
-      </nav>
+        </nav>
+      </Container>
       <div>
         <p className={styles.footer__copyright}>
           &copy; 2021. Digital Vault Services GmbH
