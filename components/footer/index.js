be0a3e9a68ea55ec59d0d038989a260457bf026a
Lia <liacicati@gmail.com>
2021-01-25 16:06:03 +0100

    fixed: list item & legal corner in footer

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,11 +1,11 @@
-import styles from './style.module.css';
-import cn from 'classnames';
-import LinkComponent from '../link';
-import Link from 'next/link';
-import Logo from '../logo';
-import navProduct from 'configs/navigation/productOverview';
-import navAbout from 'configs/navigation/aboutUs';
-import navHelp from 'configs/navigation/help';
+import styles from "./style.module.css";
+import cn from "classnames";
+import LinkComponent from "../link";
+import Link from 'next/link'
+import Logo from "../logo";
+import navProduct from "configs/navigation/productOverview";
+import navAbout from "configs/navigation/aboutUs";
+import navHelp from "configs/navigation/help";
 import navLegalCorner from 'configs/navigation/legalCorner';
 
 
@@ -55,7 +55,7 @@ const Footer = () => {
             <ul className={styles.footer__ul}>
               {navProduct.map((item) => {
                 return (
-                  <li className={styles['footer__list-item']}>
+                  <li key={item.id} className={styles['footer__list-item']}>
                     <LinkComponent
                       title={item.title}
                       href={item.href}
@@ -70,13 +70,17 @@ const Footer = () => {
           <div className={styles.footer__item}>
             <p className={styles.footer__heading}>Legal Corner</p>
             <ul className={styles.footer__ul}>
-              <li className={styles['footer__list-item']}>
-                <LinkComponent
-                  title="Legal"
-                  href="/legal"
-                  className={styles.footer__link}
-                />
-              </li>
+            {navLegalCorner.map((item) => {
+                return (
+                  <li key={item.id} className={styles['footer__list-item']}>
+                    <LinkComponent
+                      title={item.title}
+                      href={item.href}
+                      className={styles.footer__link}
+                    />
+                  </li>
+                );
+              })}
             </ul>
           </div>
 
@@ -85,7 +89,7 @@ const Footer = () => {
             <ul className={styles.footer__ul}>
               {navAbout.map((item) => {
                 return (
-                  <li className={styles['footer__list-item']}>
+                  <li key={item.id} className={styles['footer__list-item']}>
                     <LinkComponent
                       title={item.title}
                       href={item.href}
@@ -102,7 +106,7 @@ const Footer = () => {
             <ul className={styles.footer__ul}>
               {navHelp.map((item) => {
                 return (
-                  <li className={styles['footer__list-item']}>
+                  <li key={item.id} className={styles['footer__list-item']}>
                     <LinkComponent
                       title={item.title}
                       href={item.href}
