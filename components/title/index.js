3e341720414771d55e72f442a2d79bccefa5ea83
Alexei <alexei.sliusarev@gmail.com>
2021-01-26 11:11:29 +0200

    title component fixed

--- components/title/index.js
+++ components/title/index.js
@@ -1,18 +1,9 @@
 import styles from './style.module.css'
 
-const Title = (title, ) => {
-    return <h1 href={href} ref={ref}
-      className={cn(styles.btn, {
-        [styles[`btn--${background}`]]: background,
-        [styles[`btn--${textColor}`]]: textColor,
-        [styles[`btn--${size}`]]: size,
-        [styles[`btn--${border}`]]: border,
-      })}
-    >
-      {children}
-      {title}
-    </h1>
-  
+const Title = (title ) => {
+  return <h2 className={styles.sectionTitle}>
+    {title}
+    </h2>
 };
 
 export default Title;
