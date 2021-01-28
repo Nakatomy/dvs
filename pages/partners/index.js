62d6635a8c58edaa4b853e93dada4e7257429d9e
Lia <liacicati@gmail.com>
2021-01-28 21:43:24 +0100

    deleted unnecessary data

--- pages/partners/index.js
+++ pages/partners/index.js
@@ -1,33 +1,16 @@
-import PropTypes from "prop-types";
-import { i18n, withTranslation } from "i18n";
 import {
   Header,
   Section,
   Container,
-  Member,
   Title,
-  Button,
-  Partners,
   LinkComponent,
-  Testimonial,
-  Paragraph,
   Testimonials
 } from "components";
 import styles from "./style.module.css";
-import euroviaLogo from "images/partners/partner-eurovia.jpg";
+
 import partners from "configs/partners/partners";
-import Image from "next/image";
-import abstractWaves from 'images/abstract-waves.png';
-import servicesImg from 'images/services-life-cycle.svg';
-import restApiImg from 'images/solution-rest-api.svg';
-import legalImg from 'images/legal-feather.jpg';
-import lindnerLogo from 'images/partners/partner-lindner.svg'
-import eurlerHermesLogo from 'images/partners/partner-eulerhermes.svg'
-import bayernLbLogo from 'images/partners/partner-bayernlb.svg'
-import porrLogo from 'images/partners/partner-porr.jpg'
-import atradiusLogo from 'images/partners/partner-atradius.svg'
-import globalTradeLogo from 'images/partners/partner-gtc.png'
-const PartnersPage = ({ t }) => {
+
+const PartnersPage = () => {
   return (
     <>
       <Header />
@@ -56,12 +39,4 @@ const PartnersPage = ({ t }) => {
   );
 };
 
-PartnersPage.getInitialProps = async () => ({
-  namespacesRequired: ["common"],
-});
-
-//   Partners.propTypes = {
-//     t: PropTypes.func.isRequired,
-//   }
-
-export default withTranslation("common")(PartnersPage);
+export default PartnersPage
