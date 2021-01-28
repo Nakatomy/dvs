8e90ce54adc7c7757a0a8333fea7f28d3a2928c0
Alexei <alexei.sliusarev@gmail.com>
2021-01-28 17:24:21 +0200

    deleted unnecessary configs related to navigation and cleared comments in testimonial comp

--- components/testimonial/index.js
+++ components/testimonial/index.js
@@ -2,6 +2,7 @@ import styles from './style.module.css';
 import cn from 'classnames';
 
 const Testimonial = ({ children, logoSrc, logoAlt, logoWidth, author, authorTitle }) => {
+
     return (
         <div className={styles['testimonials-item']}>
             <div className={styles['testimonials__logo']}>
@@ -10,17 +11,6 @@ const Testimonial = ({ children, logoSrc, logoAlt, logoWidth, author, authorTitl
 
             {children}
 
-            {/* <p className="testimonials__paragraph">
-                    “Running a paper based manual guarantee process has always been a challenge, even more so
-                    in
-                    this Covid-19 environment. We believe that digital guarantees are the solution, they
-                    increase
-                    transparency, create efficiencies and minimize risk.
-                    </p>
-                <p className="testimonials__paragraph">
-                    The innovation that Guarantee Vault brings will benefit all parties including applicants,
-                    guarantors and beneficiaries”
-                    </p> */}
             <p className={styles['testimonials__author']}>{author}</p>
             <p className={styles['testimonials__authorTitle']}>{authorTitle}</p>
         </div>
--- configs/navigation/navigation.js
+++ configs/navigation/navigation.js
@@ -1,4 +1,7 @@
+
+
 export default [
+
 	{
 		id: 'product-overview',
 		title: 'Product Overview',
@@ -7,14 +10,14 @@ export default [
 			{ id: 'services', title: 'Services', href: '/services' },
 			{ id: 'rest-api', title: 'REST API', href: '/rest-api' }
 		]
-    },
-    {
-        id: 'legal-corner',
-        title: 'Legal Corner',
-        items: [
-            { id: 'legal', title: 'Legal', href: '/legal' }
-        ]
-    },
+	},
+	{
+		id: 'legal-corner',
+		title: 'Legal Corner',
+		items: [
+			{ id: 'legal', title: 'Legal', href: '/legal' }
+		]
+	},
 	{
 		id: 'about-us',
 		title: 'About us',
@@ -25,15 +28,16 @@ export default [
 			{ id: 'news', title: 'News', href: '/news' },
 			{ id: 'contacts', title: 'Contacts', href: '/contacts' }
 		]
-    },
-     {
-        id: 'help',
-        title: 'Help',
-        items: [
-            { id: 'privacy-policy', title: 'Privacy Policy', href: '/privacy-policy' },
-            { id: 'imprint', title: 'Imprint', href: '/imprint' }
-        ]
-    }
-    
+	},
+	{
+		id: 'help',
+		title: 'Help',
+		items: [
+			{ id: 'privacy-policy', title: 'Privacy Policy', href: '/privacy-policy' },
+			{ id: 'imprint', title: 'Imprint', href: '/imprint' }
+		]
+	}
+
+
 ];
 
