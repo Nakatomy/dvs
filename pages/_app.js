fa2614b2f805b1438a13bc0117da2b7938cf1e28
Alexei <alexei.sliusarev@gmail.com>
2021-02-01 10:12:27 +0200

    styled coockie consent

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import App from "next/app";
-import { Header, Footer, Main, Nav } from "../components";
+import { Header, Footer, Main, Nav, LinkComponent, Paragraph } from "../components";
+import Link from 'next/link'
 import "../vendor/normalize.css";
 import styles from "./style.css";
 import { appWithTranslation } from "../i18n";
@@ -13,16 +14,42 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </Main>
       <Footer />
-      <CookieConsent
+      <CookieConsent contentStyle={{ lineHeight: "1.6", maxWidth: "1500px", display: "flex", justifyItems: "center" }}
         enableDeclineButton
         flipButtons
+        declineButtonStyle={{
+          background: "none",
+          border: "solid 2px #fff",
+          color: "#fff",
+          borderRadius: "40px",
+          textTransform: "uppercase",
+          fontSize: "14px",
+          fontWeight: "700",
+          padding: "20px 32px"
+        }}
         style={{ background: "#001540", alignItems: "center" }}
-        buttonStyle={{ background: "#fff", color: "#4e503b" }}
-        buttonText="Accept cookies"
+        declineButtonText="Disable"
+        buttonStyle={{
+          background: "#00A473",
+          color: "#fff",
+          borderRadius: "40px",
+          textTransform: "uppercase",
+          fontSize: "14px",
+          fontWeight: "700",
+          padding: "20px 32px"
+        }}
+        buttonText="Continue"
+
       >
-        This website uses cookies to enhance the user experience.
+        <span >
+          We use cookies on this site to improve our service, perform analytics,
+          personalize advertising, measure advertising performance, and remember
+          website preferences. For more information on cookies visit our
+        <LinkComponent href="/privacy-policy" color="white">Privacy Policy.</LinkComponent>
+        </span>
+
       </CookieConsent>
-    </div>
+    </div >
   );
 }
 
