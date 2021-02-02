ea814e9908e396f565cd8d3df18d441e73191e61
Alexei <alexei.sliusarev@gmail.com>
2021-02-02 12:38:14 +0200

    added redirects on links used in press releases && added favicons

--- components/meta/index.js
+++ components/meta/index.js
@@ -1,8 +1,8 @@
 import Head from 'next/head'
-import favIcon from 'images/favicon.ico'
-import favIcon32 from 'images/favicon-32x32.png'
-import favIcon16 from 'images/favicon-16x16.png'
-import favIconAppleTouch from 'images/apple-touch-icon.png'
+// import favIcon from 'favicon.ico'
+import favIcon32 from 'favicon-32x32.png'
+import favIcon16 from 'favicon-16x16.png'
+import favIconAppleTouch from 'apple-touch-icon.png'
 
 const Meta = (props) => (
     <Head>
@@ -16,8 +16,7 @@ const Meta = (props) => (
         <link rel="apple-touch-icon" sizes="180x180" href={favIconAppleTouch} />
         <link rel="icon" type="image/png" sizes="32x32" href={favIcon32} />
         <link rel="icon" type="image/png" sizes="16x16" href={favIcon16} />
-
-        <link rel="icon" href={favIcon} />
+        {/* <link rel="icon" type="image/x-icon" href={favIcon} /> */}
         {/* <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content={props.title} />
         <meta name="twitter:description" content={props.desc} />
