527fe3878544a1289c379ddc324c21b459f5b27f
Lia <liacicati@gmail.com>
2021-02-03 11:46:36 +0100

    updated team member photo

--- pages/our-team/index.js
+++ pages/our-team/index.js
@@ -37,7 +37,7 @@ const Team = ({ t }) => {
             <Member src={lenaPhoto} name="Lena Kimpfbeck" job="Onboarding Manager" />
             <Member src={marinaPhoto} name="Marina Simmerl" job="PR & Marketing Manager" />
             <Member src={sabinePhoto} name="Sabine Ostermeier" job="Office Manager" />
-            <Member src={jaimePhoto} name="Julian Geddo" job="Business Analyst" />
+            <Member src={julianPhoto} name="Julian Geddo" job="Business Analyst" />
             <Member src={araPhoto} name="Ara Trujillo" job="Onboarding Manager" />
             <Member src={annaPhoto} name="Anna Mitrovic" job="Business Analyst" />
             <Member src={udoPhoto} name="Udo Holschuh" job="Onboarding Manager" />
