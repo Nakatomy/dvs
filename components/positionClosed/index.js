9a48707937bf327a5ae1a5ca09fe7fedcccc4f5c
Lia <liacicati@gmail.com>
2021-03-02 10:49:49 +0100

    added position closed component

---
+++ components/positionClosed/index.js
@@ -0,0 +1,14 @@
+import styles from "./style.module.css";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+const PositionClosed = () => {
+  return (
+    <>
+        <div className={styles.wrapper}>
+          <LockOutlinedIcon />
+          <h5 className={styles.title}>Stelle bereits besetzt</h5>
+        </div>
+    </>
+  );
+};
+
+export default PositionClosed;
